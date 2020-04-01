01823e765f17e758488b435f6faa27cd79dcb8f0
petra-at <54042171+petra-at@users.noreply.github.com>
2020-04-01 02:03:30 +0100

    Update dbh.php

--- includes/dbh.php
+++ includes/dbh.php
@@ -2,7 +2,7 @@
 
 $servername = "eu-cdbr-west-02.cleardb.net"; 
 $dbUsername="b37a6f5803ad20";
-$dbPassword="4eOjaddh7XcR0dqi";
+$dbPassword="3f91eb64";
 $dbName="heroku_bab555caa6e8527";
 
 $conn = mysqli_connect($servername,$dbUsername,$dbPassword,$dbName);
